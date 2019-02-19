f259b78c24ed394fb69403e7da5f26d483edcc9c
mjmmattoni98 <javier.magni@gmail.com>
2019-02-19 14:14:37 +0100

    Clase dirección hecha.

--- src/main/java/empresaTelefonia/Direccion.java
+++ src/main/java/empresaTelefonia/Direccion.java
@@ -1,5 +1,21 @@
 package empresaTelefonia;
 
 public class Direccion {
-    private int CP;
+    private int cp;
+    private String provincia;
+    private String poblacion;
+
+    public Direccion(){
+        super();
+    }
+
+    public Direccion(int cp, String provincia, String poblacion){
+        this.cp = cp;
+        this.provincia = provincia;
+        this.poblacion = poblacion;
+    }
+
+    public String getDireccion(){
+        return "Código postal: " + cp + "\nProvincia: " + provincia + "\nPoblación: " + poblacion ;
+    }
 }
