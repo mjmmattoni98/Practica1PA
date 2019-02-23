46c601a780b754b584860ca7fb3d00408c2e50e2
mjmmattoni98 <javier.magni@gmail.com>
2019-02-23 17:39:15 +0100

    Agregada la clase Llamada

---
+++ src/main/java/empresaTelefonia/Llamada.java
@@ -0,0 +1,15 @@
+package empresaTelefonia;
+
+public class Llamada {
+    private int numero;
+    //private Fecha fechaYHora;
+    private int duración;
+
+    public Llamada (int numero, /*Fecha fechaYHora, */int duración){
+        this.numero = numero;
+        //this.fechaYHora = fechaYHora;
+        this.duración = duración;
+    }
+
+    
+}
