8e3136820e5d9e85f1fad7a6378ae42655089882
mjmmattoni98 <javier.magni@gmail.com>
2019-02-23 17:09:23 +0100

    Agregada la clase Tarifa

---
+++ src/main/java/empresaTelefonia/Tarifa.java
@@ -0,0 +1,13 @@
+package empresaTelefonia;
+
+public class Tarifa {
+    double tarifa;
+
+    public Tarifa(double tarifa){
+        this.tarifa = tarifa;
+    }
+
+    public double getTarifa(){
+        return tarifa;
+    }
+}
