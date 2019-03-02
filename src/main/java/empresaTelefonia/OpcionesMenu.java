16f1c81d475db9c024987982c85ca90b56d1f39c
mjmmattoni98 <javier.magni@gmail.com>
2019-03-02 14:26:34 +0100

    Agregada la descripción en la enumeracion.

--- src/main/java/empresaTelefonia/OpcionesMenu.java
+++ src/main/java/empresaTelefonia/OpcionesMenu.java
@@ -1,14 +1,24 @@
 package empresaTelefonia;
 
 public enum OpcionesMenu {
-    CREAR_CUENTA,
-    BORRAR_CUENTA,
-    CAMBIAR_TARIFA,
-    MOSTRAR_DATOS_CLIENTE,
-    MOSTRAR_CLIENTES,
-    DAR_ALTA_LLAMADA,
-    MOSTRAR_LLAMADAS_CLIENTE,
-    EMITIR_FACTURA,
-    MOSTRAR_DATOS_FACTURA,
-    MOSTRAR_FACTURAS_CLIENTE;
+    CREAR_CUENTA("Crear una cuenta nueva."),
+    BORRAR_CUENTA("Borrar una cuenta."),
+    CAMBIAR_TARIFA("Cambiar la tarifa de su contrato."),
+    MOSTRAR_DATOS_CLIENTE("Mostrar los datos de un cliente."),
+    MOSTRAR_CLIENTES("Mostrar toda la lista de clientes."),
+    DAR_ALTA_LLAMADA("Agregar una llamada realizada."),
+    MOSTRAR_LLAMADAS_CLIENTE("Mostrar las llamadas realizadas por un cliente."),
+    EMITIR_FACTURA("Emitir la factura para un cliente."),
+    MOSTRAR_DATOS_FACTURA("Mostrar la informacion de una factura."),
+    MOSTRAR_FACTURAS_CLIENTE("Mostrar las facturas de un cliente.");
+
+    private String descripcion;
+
+    private OpcionesMenu(String descripcion){
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
 }
